33fea6f34943b23fc629a6588873391888881168
Sejal Dua <sejaldua@gmail.com>
2024-05-19 22:17:10 -0400

    make mile matrix

---
+++ compute-distance-matrix.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from geopy.distance import geodesic
+
+df = pd.read_csv('arena_info.csv')
+abbrev_df = pd.read_csv('abbreviations.csv')
+df = pd.merge(left=df, right=abbrev_df, left_on='Team', right_on='Franchise')
+TEAMS = sorted(df['Commonly Used Abbreviations'].to_list())
+df['Coords'] = df.apply(lambda x: (x['Latitude'], x['Longitude']), axis=1)
+coords_dict = df.set_index('Commonly Used Abbreviations')[['Coords']].to_dict(orient='index')
+
+# compute distance between each arena
+miles = []
+for t in TEAMS:
+    for tt in TEAMS:
+        distance = geodesic(coords_dict[t]['Coords'], coords_dict[tt]['Coords']).miles
+        miles.append([t, tt, distance])
+mile_df = pd.DataFrame(miles, columns = ['src', 'dest', 'miles'])
+mile_df.to_csv('mile_matrix.csv', index=False)
